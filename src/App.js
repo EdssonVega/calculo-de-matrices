9f4590c5b1344b7dc72b5f7850f6a50f756360bb
Edsson <e_d_sx2@hotmail.com>
2024-05-02 12:14:03 -0600

    finished addition section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import SumaMatrices2x2 from "./operaciones/suma/2x2/SumaMatricesDosxDos"
+import SumaMatrices3x3 from './operaciones/suma/3x3/SumaMatricesTresxTres';
+import SumaMatrices4x4 from './operaciones/suma/4x4/SumaMatricesCuatroxCuatro';
+
 
 function App() {
   const[operaciones,setOperaciones]=useState("nada");
 
   const[animation,setAnimation]=useState(false)
 
-
-  
+  const[chooseOption,setChooseOption]=useState(0)
 
   const sumita = () =>{
     setOperaciones("sumita")
@@ -25,6 +27,17 @@ function App() {
 
   const start2x2Sum = () => {
     setAnimation(!animation)
+    setChooseOption(1)
+  }
+
+  const start3x3Sum = () => {
+    setAnimation(!animation)
+    setChooseOption(2)
+  }
+
+  const start4x4Sum = () => {
+    setAnimation(!animation)
+    setChooseOption(3)
   }
 
   return (
@@ -37,8 +50,8 @@ function App() {
           {operaciones==="sumita" &&(
             <div className='sumaDosTres'> 
               <button onClick={start2x2Sum}>(2x2)</button>
-              <button>(3x3)</button>
-              <button>(4x4)</button>
+              <button onClick={start3x3Sum}>(3x3)</button>
+              <button onClick={start4x4Sum}>(4x4)</button>
             </div>
           )}
         </div>
@@ -67,7 +80,9 @@ function App() {
       </div>
       {animation === true && (
         <div className='calculusContainer'>
-          <SumaMatrices2x2 />
+          {chooseOption===1 &&(<SumaMatrices2x2 />)}
+          {chooseOption===2 &&(<SumaMatrices3x3 />)}
+          {chooseOption===3 &&(<SumaMatrices4x4 />)}
         </div>
       )}
     </div>
--- src/operaciones/suma/3x3/SumaMatricesTresxTres.js
+++ src/operaciones/suma/3x3/SumaMatricesTresxTres.js
@@ -1,22 +1,34 @@
 import React from "react";
-import "./SumaMatricesDosxDos.css";
+import "./SumaMatricesTresxTres.css";
 import { useState } from "react";
 import { TiPlusOutline } from "react-icons/ti";
 
 
 
 
-export default function SumaMatrices2x2 (){
+export default function SumaMatrices3x3 (){
     
     const[A11,setA11]=useState(0)
     const[A12,setA12]=useState(0)
+    const[A13,setA13]=useState(0)
     const[A21,setA21]=useState(0)
     const[A22,setA22]=useState(0)
+    const[A23,setA23]=useState(0)
+    const[A31,setA31]=useState(0)
+    const[A32,setA32]=useState(0)
+    const[A33,setA33]=useState(0)
+
 
     const[B11,setB11]=useState(0)
     const[B12,setB12]=useState(0)
+    const[B13,setB13]=useState(0)
     const[B21,setB21]=useState(0)
     const[B22,setB22]=useState(0)
+    const[B23,setB23]=useState(0)
+    const[B31,setB31]=useState(0)
+    const[B32,setB32]=useState(0)
+    const[B33,setB33]=useState(0)
+
 
     const[mostrarSuma,setMostrarSuma]=useState(false)
 
@@ -27,28 +39,43 @@ export default function SumaMatrices2x2 (){
     return(
         <section className="mainContainerMatrices">
             <div className="contenedorDeMatrices">
-                <div className="matriz2x2">
+                <div className="matriz3x3">
                     <input type="number" onChange={(e) => {setA11(parseInt(e.target.value))}}></input>
                     <input type="number" onChange={(e) => {setA12(parseInt(e.target.value))}}></input>
+                    <input type="number" onChange={(e) => {setA13(parseInt(e.target.value))}}></input>
                     <input type="number" onChange={(e) => {setA21(parseInt(e.target.value))}}></input>
                     <input type="number" onChange={(e) => {setA22(parseInt(e.target.value))}}></input>
+                    <input type="number" onChange={(e) => {setA23(parseInt(e.target.value))}}></input>
+                    <input type="number" onChange={(e) => {setA31(parseInt(e.target.value))}}></input>
+                    <input type="number" onChange={(e) => {setA32(parseInt(e.target.value))}}></input>
+                    <input type="number" onChange={(e) => {setA33(parseInt(e.target.value))}}></input>
                 </div>
                 <TiPlusOutline />
-                <div className="matriz2x2">
+                <div className="matriz3x3">
                     <input type="number" onChange={(e) => {setB11(parseInt(e.target.value))}}></input>
                     <input type="number" onChange={(e) => {setB12(parseInt(e.target.value))}}></input>
+                    <input type="number" onChange={(e) => {setB13(parseInt(e.target.value))}}></input>
                     <input type="number" onChange={(e) => {setB21(parseInt(e.target.value))}}></input>
                     <input type="number" onChange={(e) => {setB22(parseInt(e.target.value))}}></input>
+                    <input type="number" onChange={(e) => {setB23(parseInt(e.target.value))}}></input>
+                    <input type="number" onChange={(e) => {setB31(parseInt(e.target.value))}}></input>
+                    <input type="number" onChange={(e) => {setB32(parseInt(e.target.value))}}></input>
+                    <input type="number" onChange={(e) => {setB33(parseInt(e.target.value))}}></input>
                 </div>
                 <button className="addMatriz">añadir matriz</button>
             </div>
             <button className="calculateButton" onClick={calcularSuma}>CALCULAR</button>
             {mostrarSuma ===true &&(
-            <div className="matriz2x2Resultado">
+            <div className="matriz3x3Resultado">
                 <h1>{A11+B11}</h1>
                 <h1>{A12+B12}</h1>
+                <h1>{A13+B13}</h1>
                 <h1>{A21+B21}</h1>
                 <h1>{A22+B22}</h1>
+                <h1>{A23+B23}</h1>
+                <h1>{A31+B31}</h1>
+                <h1>{A32+B32}</h1>
+                <h1>{A33+B33}</h1>
             </div>
             )}
         </section>
