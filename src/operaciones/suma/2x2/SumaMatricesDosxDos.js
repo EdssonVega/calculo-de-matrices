b407fbecfc87ed5c793796f390d23151e80dcead
Edsson <e_d_sx2@hotmail.com>
2024-05-02 11:20:44 -0600

    added math logic

--- src/operaciones/suma/2x2/SumaMatricesDosxDos.js
+++ src/operaciones/suma/2x2/SumaMatricesDosxDos.js
@@ -7,31 +7,48 @@ import { useState } from "react";
 
 export default function SumaMatrices2x2 (){
     
-    const[number11,setNumber11]=useState([])
+    const[A11,setA11]=useState(0)
+    const[A12,setA12]=useState(0)
+    const[A21,setA21]=useState(0)
+    const[A22,setA22]=useState(0)
 
-    const handleInputNumber11 = (target) => {
-        setNumber11(target.value)
+    const[B11,setB11]=useState(0)
+    const[B12,setB12]=useState(0)
+    const[B21,setB21]=useState(0)
+    const[B22,setB22]=useState(0)
+
+    const[mostrarSuma,setMostrarSuma]=useState(false)
+
+    const calcularSuma = () => {
+        setMostrarSuma(!mostrarSuma)
     }
-    console.log(number11)
 
     return(
         <section className="mainContainerMatrices">
             <div className="contenedorDeMatrices">
                 <div className="matriz2x2">
-                    <input type="number" onChange={handleInputNumber11}></input>
-                    <input type="number"></input>
-                    <input type="number"></input>
-                    <input type="number"></input>
+                    <input type="number" onChange={(e) => {setA11(parseInt(e.target.value))}}></input>
+                    <input type="number" onChange={(e) => {setA12(parseInt(e.target.value))}}></input>
+                    <input type="number" onChange={(e) => {setA21(parseInt(e.target.value))}}></input>
+                    <input type="number" onChange={(e) => {setA22(parseInt(e.target.value))}}></input>
                 </div>
                 <div className="matriz2x2">
-                    <input type="number"></input>
-                    <input type="number"></input>
-                    <input type="number"></input>
-                    <input type="number"></input>
+                    <input type="number" onChange={(e) => {setB11(parseInt(e.target.value))}}></input>
+                    <input type="number" onChange={(e) => {setB12(parseInt(e.target.value))}}></input>
+                    <input type="number" onChange={(e) => {setB21(parseInt(e.target.value))}}></input>
+                    <input type="number" onChange={(e) => {setB22(parseInt(e.target.value))}}></input>
                 </div>
                 <button className="addMatriz">añadir matriz</button>
             </div>
-            <button className="calculateButton">CALCULAR</button>
+            <button className="calculateButton" onClick={calcularSuma}>CALCULAR</button>
+            {mostrarSuma ===true &&(
+            <div className="matriz2x2Resultado">
+                <h1>{A11+B11}</h1>
+                <h1>{A12+B12}</h1>
+                <h1>{A21+B21}</h1>
+                <h1>{A22+B22}</h1>
+            </div>
+            )}
         </section>
     )
 }
